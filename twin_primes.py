0a484280a44387f58b87a2d1e07d1d5f99f841da
Halil Çetiner <hhalilcetiner@gmail.com>
2018-01-25 18:32:50 +0300

    Working bot launched at twitter/@_twin_primes

---
+++ twin_primes.py
@@ -0,0 +1,36 @@
+import json
+import tweepy, time, sys
+
+def parse_primes(file):
+    file = open(file, "r")
+    data_file = file.read()
+    data = []
+    curStr = ''
+    for c in data_file:
+        if c.isdigit():
+            curStr = curStr + c
+        elif curStr != '':
+            data.append(curStr)
+            curStr = ''
+        else:
+            curStr = ''
+    
+    return ','.join(data)
+
+twin_primes = parse_primes("twin_primes.txt")
+
+CONSUMER_KEY = '***'
+CONSUMER_SECRET = '***'
+ACCESS_TOKEN = '***'
+ACCESS_TOKEN_SECRET = '***'
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+data = twin_primes.split(',')
+
+last_tweet = api.user_timeline(screen_name = '_twin_primes',count=1)[0].text
+last_posted_elem = last_tweet.split(',')[0]
+next_index = data.index(last_posted_elem) + 1
+twit = data[next_index] + ', ' + str(int(data[next_index]) + 2)
+api.update_status(twin)
